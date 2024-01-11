1be6ef7871509c6e9b59dc4de5b777fe58b6e152
iitenkie <846708308gm@gmail.com>
2024-01-11 22:46:34 +0800

    upd

--- pages/images/daily/index.vue
+++ pages/images/daily/index.vue
@@ -4,21 +4,21 @@
     <section class="flex flex:col">
       <div class="flex flex:col gap:16 align-items:center">
         <NuxtLink
-          v-for="image in data?.items"
-          :to="`/images/daily/${image[0]}`"
+          v-for="collection in data?.items"
+          :to="`/images/daily/${collection[0]}`"
           class="general-width w:full"
         >
           <article
             class="shadow-1 r:6 p:16 mt:16 bg:white translateY(-0.5rem):hover ~transform|250ms|ease"
           >
             <div class="f:1.5em f:bold mt:-32 mb:8 text-decoration">
-              {{ image[0] }}
+              {{ collection[0] }}
             </div>
             <div
-              class="w:full h:150 px:16 py:8 flex flex:row gap:8 overflow-x:hidden b:2 b:dashed b:var(--surface-border) r:6 bg:var(--surface-ground)"
+              class="rel w:full h:150 px:16 py:8 flex flex:row gap:8 overflow-x:hidden b:2 b:dashed b:var(--surface-border) r:6 bg:var(--surface-ground)"
             >
               <div
-                v-for="file in image[1].slice(0, 6)"
+                v-for="file in collection[1].slice(0, 6)"
                 class="h:full r:6 shadow-2 aspect:1/1 bg:center bg:cover"
                 :class="{ 'filter:blur(10px)': file.nsfw }"
                 :style="{
@@ -27,6 +27,10 @@
                   )}')`,
                 }"
               ></div>
+              <div class="abs bottom:0 right:0 flex gap:8 align-items:center color:white bg:rgba(0,0,0,0.4) rtl:6 p:8 shadow-2">
+                <i class="pi pi-image"></i>
+                <span>{{ collection[1].length }}</span>
+              </div>
             </div>
           </article>
         </NuxtLink>
@@ -48,8 +52,7 @@
 import BlockUI from "primevue/blockui";
 import Paginator from "primevue/paginator";
 
-import { groupBy } from "lodash-es";
-import { sortMetaArrayToFormat, SiteData } from "~/utils";
+import { SiteData } from "~/utils";
 
 import type { DataTableSortMeta } from "primevue/datatable";
 
